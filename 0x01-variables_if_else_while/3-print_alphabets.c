fc90727dffafa4c62939bcd5aa2c30a95b23731d
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-12 09:36:29 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - starting point
+ *
+ * Return: always 0
+ */
+int main (void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
