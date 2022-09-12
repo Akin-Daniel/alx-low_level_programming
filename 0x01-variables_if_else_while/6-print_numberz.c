3fdd8ed10c59ceeb2031a29bcdb976191597aeb1
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-12 10:06:38 +0000

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned int a = 0;
+	for (; a < 10; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return(0);
+}
