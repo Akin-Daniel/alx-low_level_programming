92f16a49cc9b9a31927b9826ecf92993218b2634
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-12 09:46:59 +0000

    special alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - starting point
+ *
+ * Return: always 0
+ */
+int main (void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
