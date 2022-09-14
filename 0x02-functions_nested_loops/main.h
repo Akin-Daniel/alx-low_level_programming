0d17152424b627714d62e918d9352b611368225b
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-14 10:02:52 +0000

    main header corrected

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,12 @@
-#include <unistd.h>
-
+#ifndef _MAIN_H
+#define _MAIN_H
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+ * _putchar - writes the character c to std
+ * @C: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/*
+ * print_alphabet - function to print alphabet
+ *
+ */
