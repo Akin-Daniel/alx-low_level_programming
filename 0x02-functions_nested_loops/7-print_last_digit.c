d2e4643e42fb45962e41d6001b537048c18eb508
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-14 17:39:23 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ *
+ * @x: the input variable
+ *
+ * Return: r
+ */
+int print_last_digit(int x)
+{
+	int r;
+
+	r = x % 10;
+
+	return (r);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -33,4 +33,8 @@ int print_sign(int n);
  *
  */
 int _abs(int);
+/**
+ * print_last_digit - prints the last digit of any input
+ */
+int print_last_digit(int);
 #endif
