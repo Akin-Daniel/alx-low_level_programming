9291b0480979fb9344a09f566898a0de917d740b
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-14 21:25:20 +0000

    changed am

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - starting  point
+ *
+ * @x: 1st variable
+ * @y: 2nd variable
+ *
+ * return: returns the result
+ */
+int add(int x, int y);
+{
+	int result = x + y;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -45,4 +45,9 @@ void jack_bauer(void);
  * times_table - prints the  9 times table
  */
 void times_table(void);
+/**
+ * add - computes the sum of two numbers
+ *
+ */
+int add(int,  int);
 #endif
