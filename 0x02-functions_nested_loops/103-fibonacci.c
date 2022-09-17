59fde19a92a68160d5e835c8becf23cd5aefdf65
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-17 13:57:38 +0000

    fibonacci cracked

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio>
+/**
+ * main - starting point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int n = 0;
+
+	for (i = 1; i < 32; i++)
+	{
+		if (b % 2 == 0)
+		{
+			n += b;
+			if (a % 2 == 0)
+			{
+				n += a;
+			}
+		}
+		b += a;
+		a = b - a;
+	}
+	printf("%lu \n", n);
+	return (0);
+}
