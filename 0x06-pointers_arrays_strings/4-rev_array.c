020372c4c9de0a80b16f0e4ecb891f760091c9eb
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-23 21:26:22 +0000

    mama

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,7 @@
  * @a: array
  * @n: number of elements of the  array
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int tmp, beg = 0;
 	int end = n - 1; /* omit null terminator in length */
