daf732ce7bde81a66aae18e31cdd496e2aed1a15
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-23 21:22:17 +0000

    manna

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content  of an array of integers
+ * @a: array
+ * @n: number of elements of the  array
+ */
+void reverse_array(int *a, int n);
+{
+	int tmp, beg = 0;
+	int end = n - 1; /* omit null terminator in length */
+
+	while (beg < end)
+	{
+		tmp = *(a + beg);
+		*(a + beg) = *(a + end);
+		*(a + end) = tmp;
+		beg++, end--;
+	}
+}
