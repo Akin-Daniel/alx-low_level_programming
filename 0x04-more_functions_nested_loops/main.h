948dfce29df8dab85f2fbb0a2e911039226541a3
Akin-Daniel <akinsdanny05@gmail.com>
2022-09-27 11:07:35 +0000

    mama

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar - writes the character c to std
+ * @C: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * -isupper - checks for uppercase character
+ *  @c: parameter to compare
+ *  Return: 1 or 0
+ */
+int _isupper(int c);
+
+#endif
