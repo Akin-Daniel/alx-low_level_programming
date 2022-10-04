287ca77174dd0c70cc1071c195bbfa0ed3a99537
Akin-Daniel <akinsdanny05@gmail.com>
2022-10-04 10:24:42 +0000

    corrected

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- * _puts_recursion - prints a string
- * @*s: the string
+ * _puts_recursion - print string with new line
+ * @s: string to print
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
